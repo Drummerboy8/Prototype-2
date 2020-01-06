0d5fbcb1435231ad9e0108389b1a1bda803978ce
Darien Turner <56174615+Drummerboy8@users.noreply.github.com>
2020-01-06 13:17:48 -0800

    yes
    yes

--- Assets/playercontrollee.cs
+++ Assets/playercontrollee.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class playercontrollee : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f; 
+    public float xRange = 10.0f; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,15 @@ public class playercontrollee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
     }
 }
